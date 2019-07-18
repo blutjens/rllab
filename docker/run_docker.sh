220c6c909588bcbb4c765f6eb0dd821d7bfb5461
Bjoern Luetjens <bjoern.luetjens@TIGGER-D.bluerivertech.com>
2019-07-18 14:49:14 -0700

    changed run docker to have path of desktop

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -5,7 +5,7 @@
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   --name rllab \
   -p 8888:8888 -p 6006:6006 \
-  -v /home/$USER/:/home/$USER \
+  -v /home/bluerivertech/$USER/:/home/$USER \
   -v /dev/serial:/dev/serial \
   lutjens/rllab:v0
 
@@ -18,7 +18,7 @@
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   --name rllab \
   -p 8888:8888 -p 6006:6006 \
-  -v /home/$USER/:/home/$USER \
+  -v /home/bluerivertech/$USER/:/home/$USER \
   --device=/dev/serial/by-id/usb-FTDI_FT231X_USB_UART_DO00FZYH-if00-port0 \
   lutjens/rllab:v0
 }
